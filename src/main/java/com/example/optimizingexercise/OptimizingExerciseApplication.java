4da62429e2b20e7660ac2493d5e725b8d00b020a
twg0 <fruitsjh@gmail.com>
2023-11-03 22:34:18 +0900

    Initial commit

---
+++ src/main/java/com/example/optimizingexercise/OptimizingExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.optimizingexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizingExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimizingExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/optimizingexercise/OptimizingExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.optimizingexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimizingExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
