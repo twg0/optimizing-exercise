8de5b9c69055a8c78af2acc8407c2f53cfac7945
twg0 <fruitsjh@gmail.com>
2023-11-09 00:28:07 +0900

    feat : CLub Entity 설정

---
+++ src/main/java/com/example/optimizingexercise/entity/Club.java
@@ -0,0 +1,29 @@
+package com.example.optimizingexercise.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Club {
+	@Id @GeneratedValue
+	private Long id;
+
+	private String clubName;
+
+	/* 연관 관계 */
+	@OneToMany(mappedBy = "club")
+	private List<Member> members = new ArrayList<>();
+
+	@OneToMany(mappedBy = "club")
+	private List<Account> accounts = new ArrayList<>();
+}
