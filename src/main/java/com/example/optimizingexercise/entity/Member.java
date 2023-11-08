3b8552f3ae40782494d59770033a45bcdbd616fc
twg0 <fruitsjh@gmail.com>
2023-11-09 00:28:32 +0900

    feat : Member 연관관계 추가

--- src/main/java/com/example/optimizingexercise/entity/Member.java
+++ src/main/java/com/example/optimizingexercise/entity/Member.java
@@ -1,9 +1,16 @@
 package com.example.optimizingexercise.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,4 +36,10 @@ public class Member {
 	private Integer assist;
 
 	/* 연관 관계 */
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "club_id")
+	private Club club;
+
+	@OneToMany(mappedBy = "member")
+	private List<Account> accounts = new ArrayList<>();
 }
