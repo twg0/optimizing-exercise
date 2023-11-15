866836a6b1e51ede4c3864405a8015edb74303be
twg0 <fruitsjh@gmail.com>
2023-11-15 23:49:02 +0900

    feat : 메소드 추가 - entity clubName - boolean clubName

--- src/main/java/com/example/optimizingexercise/repository/ClubRepository.java
+++ src/main/java/com/example/optimizingexercise/repository/ClubRepository.java
@@ -1,5 +1,7 @@
 package com.example.optimizingexercise.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,8 @@ import com.example.optimizingexercise.entity.Club;
 
 @Repository
 public interface ClubRepository extends JpaRepository<Club, Long> {
+
+	Optional<Club> findByClubName(String clubName);
+
+	boolean existsByClubName(String clubName);
 }
