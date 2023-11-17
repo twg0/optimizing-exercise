b42cb5074d2c851c386f8a51a737f89b14c4aa01
twg0 <fruitsjh@gmail.com>
2023-11-17 23:51:00 +0900

    feat : accountNumber 기준 검색 메소드 생성

--- src/main/java/com/example/optimizingexercise/repository/AccountRepository.java
+++ src/main/java/com/example/optimizingexercise/repository/AccountRepository.java
@@ -1,5 +1,7 @@
 package com.example.optimizingexercise.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.example.optimizingexercise.entity.Account;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+	Optional<Account> findByAccountNumber(String accountNumber);
 }
